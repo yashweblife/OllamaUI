e7ed0dbe5d35e14f7564107809c085e70422355a
yashweblife <yashweblife20@gmail.com>
2025-08-07 16:06:11 -0500

    add highlighting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import 'github-markdown-css/github-markdown-dark.css'
 import { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import './App.css'
 function App() {
   const [text, setText] = useState('')
-  const test = async () =>{
-    const res = await fetch('http://localhost:11434/api/generate',{
+  const test = async () => {
+    const res = await fetch('http://localhost:11434/api/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model:"gpt-oss:20b",
-        prompt:"write a simple js code to print hello world",
-        stream:false
+        model: "gpt-oss:20b",
+        prompt: "write a simple js code to print hello world",
+        stream: false
       })
     })
     const data = await res.json()
@@ -21,7 +24,26 @@ function App() {
   }
   return (
     <div className="App">
-      <ReactMarkdown >
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={vscDarkPlus}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      >
         {text || ''}
       </ReactMarkdown>
       <button onClick={test}>Click</button>
