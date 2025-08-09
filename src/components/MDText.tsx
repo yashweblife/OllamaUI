647f8bd97b21af1c2212b547865642745705672a
yashweblife <yashweblife20@gmail.com>
2025-08-09 10:53:04 -0500

    new component

---
+++ src/components/MDText.tsx
@@ -0,0 +1,37 @@
+import ReactMarkdown from "react-markdown"
+import SyntaxHighlighter from "react-syntax-highlighter"
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
+
+export default function MDText({ text, owner }: { text: string, owner: string }) {
+    return (
+
+        <div
+            style={{
+                backgroundColor: 'rgb(22,22,22)',
+                padding: '1rem',
+                borderRadius: '0.5rem',
+            }}
+        >
+            <ReactMarkdown
+                components={{
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || '')
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                children={String(children).replace(/\n$/, '')}
+                                style={vscDarkPlus}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            />
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        )
+                    }
+                }}
+            >{text}</ReactMarkdown>
+        </div>
+    )
+}
